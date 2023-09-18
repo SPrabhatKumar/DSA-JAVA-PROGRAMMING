db671d523e16914ee17c7663e452cd822233f6ab
S.Prabhat Kumar <itsmeprabhatkumar4u@gmail.com>
2023-09-18 15:13:44 +0530

    InsertionSort Update

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -1,29 +1,25 @@
+import java.util.Arrays;
 import java.util.Scanner;
 // Insertion Sort Time Complexoty 
 
 public class InsertionSort {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter The Array Size :");
-        int size = sc.nextByte();
-        int arr[] = new int[size];
-        System.out.print("\n Enter The Array Element :");
-        for (int i = 0; i < size; i++) {
-            arr[i] = sc.nextInt();
-        }
-        for (int i = 1; i < arr.length; i++) {
-            int current = arr[i];
-            int j = i - 1;
-            while (j >= 0 & current < arr[j]) {
-                arr[j + 1] = arr[j];
-                j--;
+        int arr[] = { 65, 4, 32, 4, 2, 1, 10 };
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i+1; j>0; j--) {
+                if(arr[j]<arr[j-1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j-1];
+                    arr[j-1]=temp;
+                }
+                else{
+                    break;
+                }
+                
             }
-            arr[j + 1] = current;
-
         }
-        for (int i = 0; i < size; i++) {
-            System.out.print(" " + arr[i]);
-        }
-
+        System.out.println(Arrays.toString(arr));
     }
+
+   
 }
